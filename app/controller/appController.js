29ada6191f06ba8b1a88ecbfd0c56437ba7461ba
viktor <ovodessa@gmail.com>
2021-07-02 12:28:36 -0500

    added jwt tokens feature to protect the routes

--- app/controller/appController.js
+++ app/controller/appController.js
@@ -2,7 +2,10 @@
 
 var Temp = require('../model/appModel.js');
 
-exports.getTemp = function(req, res) {
+exports.getTemp = function(req, res) { console.log(req.user);
+  if(req.user === undefined) {
+    res.sendStatus(401)
+  } else {
     Temp.getTemp(function(err, task) {
       console.log('controller')
       if (err) {
@@ -11,6 +14,7 @@ exports.getTemp = function(req, res) {
       console.log('res', task);
       res.send(task);
     });
+  }
 };
 
 exports.getMeanTempByDate = function(req, res) {
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express'),
   app = express(),
+  jsonwebtoken = require("jsonwebtoken"),
   bodyParser = require('body-parser');
   port = process.env.PORT || 8000;
 
@@ -17,6 +18,25 @@ const mc = mysql.createConnection({
 mc.connect();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(function(req, res, next) {console.log(req.headers.authorization);
+  if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[1] === 'JWT') {
+
+    jsonwebtoken.verify(req.headers.authorization.split(' ')[2], 'JWT_SECRET', function(err, decode) {
+      if (err) {
+		    console.log(err.message);
+		    req.user = undefined;
+	    } else {
+        req.user = decode;
+      }
+      next();
+    });
+  } else {
+    console.log("NO JWT");
+    req.user = undefined;
+    next();
+  }
+});
 
 var routes = require('./app/routes/appRoutes'); //importing route
 routes(app); //register the route
